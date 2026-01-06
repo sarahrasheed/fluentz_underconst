bd725b106d06d4c0dc185d0a310e1d1ab2be1776
sarahrasheed <41295727+sarahrasheed@users.noreply.github.com>
2026-01-06 19:48:29 +0200

    Delete backend/create_tables.py

--- backend/create_tables.py
+++
@@ -1,9 +0,0 @@
-from app.db import engine
-from app.models import Base  # wherever your SQLAlchemy Base is defined
-
-def main():
-    Base.metadata.create_all(bind=engine)
-    print("✅ Tables created successfully.")
-
-if __name__ == "__main__":
-    main()
