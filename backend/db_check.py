ee09b3e143b44b0be09515378cca0953e8f8e436
sarahrasheed <41295727+sarahrasheed@users.noreply.github.com>
2026-01-06 19:48:40 +0200

    Delete backend/db_check.py

--- backend/db_check.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import text
-from app.db import engine
-
-with engine.connect() as conn:
-    print("✅ Connected to DB")
-    db_name = conn.execute(text("SELECT DATABASE()")).scalar()
-    print("DB:", db_name)
-    tables = conn.execute(text("SHOW TABLES")).fetchall()
-    print("Tables:", tables)
